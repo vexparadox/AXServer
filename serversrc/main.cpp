7400af29acf5b752b723859c2cf390d7e188f3be
William Meaton <will.meaton@gmail.com>
2016-07-24 16:12:10 +0100

    Added better description of server creation

--- serversrc/main.cpp
+++ serversrc/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 {
     //start a new runner a server
     //Servers will auto to 127.0.0.1:1234 with 12 max clients, 2 channels,  and unlimited incoming and outgoing bandwidth 
-    //you can define a custom one new Server("x.x.x.x", 80, 3, 2, 0, 0);
+    //you can define a custom one new Server("192.168.0.1", 80, 3, 2, 256, 512);
     //Server(std::string& address, int port, int maxClients, int numChannels, int incBandwidth, int outBandwidth);
     Runner(new Server());
     return 0;
